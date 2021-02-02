15d7349c20f1c6c8a6cd8cfe3ca24f87d65e209b
hieut <hieudev96@gmail.com>
2021-02-02 17:15:20 +0700

    update config

--- common/layout/Header.js
+++ common/layout/Header.js
@@ -10,7 +10,7 @@ import { getStrapiMedia } from "../../lib/media";
 
 export default function Header() {
   const setStatus = useSetRecoilState(statusMenubar);
-  const { favicon } = useContext(GlobalContext);
+  const { favicon, logo } = useContext(GlobalContext);
   const imageUrl = getStrapiMedia(favicon);
 
 
@@ -21,50 +21,13 @@ export default function Header() {
             <div className="row d-flex">
               <div className="col-lg-3 col-md-3 header-logo">
                 <a href="/" title="Logo">
-                  <img src="images/logo.png" alt="ND Tool" />
+                  <img src={getStrapiMedia(logo)} alt="logo do bao ho"  style={{paddingRight: '25px'}}/>
                 </a>
               </div>
               <div className="col-lg-9 col-md-9 header-content">
                 <div className="header-topbar">
                   <div className="left">
-                    Chào mừng bạn đến với thiết bị cơ khí ND!
-                  </div>
-                  <div className="right">
-                    <div className="block-search">
-                      <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" viewBox="0 0 192.904 192.904" style={{enableBackground: 'new 0 0 192.904 192.904'}} xmlSpace="preserve">
-                        <path d="M190.707,180.101l-47.078-47.077c11.702-14.072,18.752-32.142,18.752-51.831C162.381,36.423,125.959,0,81.191,0  C36.422,0,0,36.423,0,81.193c0,44.767,36.422,81.187,81.191,81.187c19.688,0,37.759-7.049,51.831-18.751l47.079,47.078  c1.464,1.465,3.384,2.197,5.303,2.197c1.919,0,3.839-0.732,5.304-2.197C193.637,187.778,193.637,183.03,190.707,180.101z M15,81.193  C15,44.694,44.693,15,81.191,15c36.497,0,66.189,29.694,66.189,66.193c0,36.496-29.692,66.187-66.189,66.187  C44.693,147.38,15,117.689,15,81.193z" />
-                      </svg>
-                      <div className="header_search search_form">
-                        <form className="input-group search-bar search_form" action="/search" method="get" role="search">		
-                          <input type="search" name="query" defaultValue placeholder="Tìm kiếm..." className="change_pl input-group-field st-default-search-input search-text" autoComplete="off" />
-                          <span className="input-group-btn">
-                            <button className="btn icon-fallback-text">
-                              <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" viewBox="0 0 192.904 192.904" style={{enableBackground: 'new 0 0 192.904 192.904'}} xmlSpace="preserve">
-                                <path d="M190.707,180.101l-47.078-47.077c11.702-14.072,18.752-32.142,18.752-51.831C162.381,36.423,125.959,0,81.191,0  C36.422,0,0,36.423,0,81.193c0,44.767,36.422,81.187,81.191,81.187c19.688,0,37.759-7.049,51.831-18.751l47.079,47.078  c1.464,1.465,3.384,2.197,5.303,2.197c1.919,0,3.839-0.732,5.304-2.197C193.637,187.778,193.637,183.03,190.707,180.101z M15,81.193  C15,44.694,44.693,15,81.191,15c36.497,0,66.189,29.694,66.189,66.193c0,36.496-29.692,66.187-66.189,66.187  C44.693,147.38,15,117.689,15,81.193z" />
-                              </svg>
-                            </button>
-                          </span>
-                        </form>
-                      </div>
-                    </div>
-                    <div className="block-login">
-                      <a href="/account" title="Tài khoản" rel="nofollow">
-                        <svg viewBox="0 0 512 512"><path d="M437.02,330.98c-27.883-27.882-61.071-48.523-97.281-61.018C378.521,243.251,404,198.548,404,148 C404,66.393,337.607,0,256,0S108,66.393,108,148c0,50.548,25.479,95.251,64.262,121.962 c-36.21,12.495-69.398,33.136-97.281,61.018C26.629,379.333,0,443.62,0,512h40c0-119.103,96.897-216,216-216s216,96.897,216,216 h40C512,443.62,485.371,379.333,437.02,330.98z M256,256c-59.551,0-108-48.448-108-108S196.449,40,256,40 c59.551,0,108,48.448,108,108S315.551,256,256,256z" data-original="#222222" className="active-path" fill="#222222" /></svg>
-                        Tài khoản
-                      </a>
-                      <ul>
-                        <li>
-                          <a rel="nofollow" href="/account/login" title="Đăng nhập">
-                            Đăng nhập
-                          </a>
-                        </li>
-                        <li>
-                          <a rel="nofollow" href="/account/register" title="Đăng ký">
-                            Đăng ký
-                          </a>
-                        </li>
-                      </ul>
-                    </div>
+                    Chào mừng bạn đến với đồ bảo hộ Hiếu Trần, chúng tôi chuyên cung cấp sỉ,lẻ các sản phẩm về đồ bảo hộ.
                   </div>
                 </div>
                 <div className="header-mid">
@@ -172,14 +135,6 @@ export default function Header() {
                               <li className="nav-item ">				
                                 <a className="a-img" href="/lien-he" title="Liên hệ">
                                   <span>Liên hệ</span>
-                                  <span className="label_">
-                                    <i className="label " />
-                                  </span>
-                                </a>
-                              </li>
-                              <li className="nav-item ">				
-                                <a className="a-img" href="/dai-ly" title="Đại lý">
-                                  <span>Đại lý</span>
                                   <span className="label_">
                                     <i className="label " />
                                   </span>
